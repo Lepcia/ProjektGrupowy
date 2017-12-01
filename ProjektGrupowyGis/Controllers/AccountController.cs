68cbf0809620db2da5fcfb0ed20f552808f8ec4a
Daria Lepa <daria@pwrs.pl>
2017-12-01 17:12:29 +0100

    Xml database and user registration

--- ProjektGrupowyGis/Controllers/AccountController.cs
+++ ProjektGrupowyGis/Controllers/AccountController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Data.Helper;
+using Data.Helper.External.Models;
+using ProjektGrupowyGis.Models;
+using System.Threading.Tasks;
 
 namespace ProjektGrupowyGis.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
-        // GET: Account
-        public ActionResult Index()
+        [AllowAnonymous]
+        public ActionResult Register()
         {
             return View();
+        }
+
+        public AccountController()
+        { }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid) {
+                XMLReader _xmlReader = new XMLReader();
+                XMLWriter _xmlWriter = new XMLWriter();
+
+                var loginTaken = _xmlReader.CheckIfLoginTaken(model.Login);
+                if (!loginTaken){
+                    _xmlWriter.InsertUser(model.Login, model.Name, model.Email, model.Password);
+                }
+                else {
+                    ModelState.AddModelError("LoginTaken", "Login already taken.");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ ProjektGrupowyGis/Models/AccountViewModels.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ProjektGrupowyGis.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="{0} is required!")]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!!")]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!")]
+        [StringLength(100, ErrorMessage = "{0} musi mieć przynajmniej {2} znaków", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
